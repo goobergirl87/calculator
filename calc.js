125b935215ba175dee24798824c0c40fca902774
goobergirl87 <isabelheidekorn@gmail.com>
2023-04-25 10:54:40 +0200

    Create number and operator variables

--- calc.js
+++ calc.js
@@ -2,3 +2,7 @@ const add = (num1,num2) => num1 + num2;
 const sub = (num1,num2) => num1 - num2;
 const mul = (num1,num2) => num1 * num2;
 const div = (num1,num2) => num1 / num2;
+
+let num1 = 0;
+let num2 = 0;
+let op;
