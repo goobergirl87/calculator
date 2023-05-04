24c7dfc087bca29a83dde0887d91c7f07c1270a0
goobergirl87 <isabelheidekorn@gmail.com>
2023-05-04 13:34:01 +0200

    Add function calculate

--- calc.js
+++ calc.js
@@ -49,6 +49,22 @@ function handleOperator (op) {
     currentValue = "";
 }
 
+function calculate () {
+    previousValue = Number(previousValue) //convert string into a number
+    currentValue = Number(currentValue) //convert string into a number
+
+    if(operator === "+") {
+        previousValue += currentValue;
+    } else if(operator === "-") {
+        previousValue -= currentValue;
+    } else if(operator === "*") {
+        previousValue *= currentValue;
+    } else{
+        previousValue /= currentValue;
+    }
+    console.log(previousValue)
+}
+
 const add = (num1,num2) => num1 + num2;
 const sub = (num1,num2) => num1 - num2;
 const mul = (num1,num2) => num1 * num2;
